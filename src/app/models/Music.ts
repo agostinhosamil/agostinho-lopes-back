7aff02ce0671997966727aeb3a44b5f75fd957fa
Agostinho Sam'l <agostinhosaml832@gmail.com>
2021-12-11 22:21:53 +0100

    fix: move the all, search and findById method to the Model Parent class

--- src/app/models/Music.ts
+++ src/app/models/Music.ts
@@ -12,43 +12,5 @@ export interface MusicRecord {
 }
 
 export class Music extends AppModel<MusicTypes> {
-  protected tableName = 'musics'
-  protected static tableName = 'musics'
-
   protected requireds = ['name']
-  
-  static async all (): Promise<MusicTypes[] | void> {    
-    const musicList = await knex<MusicTypes> (this.tableName)
-      .select ('*')
-    
-    return musicList
-  }
-
-  static async search (musicQueryDatas: MusicTypes): Promise<MusicTypes[]> {
-
-    const query = knex (this.tableName)
-      .select ('*')
-
-    Object.keys (musicQueryDatas).map ((key, index) => {
-      const value = `%${musicQueryDatas [key as keyof MusicTypes]}%`
-
-      if (index === 0) {
-        query.where (key, 'like', value)
-      } else {
-        query.andWhere (key, 'like', value)
-      }
-    })
-
-    const queryResultList = await query
-
-    return queryResultList
-  }
-
-  static async findById (id: number): Promise<MusicTypes> {    
-    const musicList = await knex (this.tableName)
-      .select ('*')
-      .where ({ id })
-    
-    return musicList.length >= 1 ? musicList [0] : null
-  }
 }
