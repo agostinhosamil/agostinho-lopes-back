962b3d9d3fc0c22b3c2c5cc476bee1e9fd9bfddb
agostinhosamil <agostinhosaml832@gmail.com>
2021-12-11 13:37:09 +0100

    feat(app): create the store, autocomplete and show actions

--- src/app/controllers/MusicController.ts
+++ src/app/controllers/MusicController.ts
@@ -1,1 +1,38 @@
-export class MusicController {}
+import { Request, Response } from 'express'
+
+import { Music, MusicRecord, MusicTypes } from '@models/Music'
+
+interface RequestParamList {
+  id: number
+  music_name: string
+}
+export class MusicController {
+  async index (req: Request, res: Response): Promise<Response> {
+
+    const list = await Music.all ();
+
+    return res.send (list)
+  }
+
+  async show (req: Request<RequestParamList, MusicTypes>, res: Response<MusicTypes>): Promise<Response> {
+    const music = await Music.findById (req.params.id)
+    
+    return res.send (music);
+  }
+
+  async autocomplete (req: Request<RequestParamList, MusicTypes[]>, res: Response<MusicTypes[]>): Promise<Response> {
+    const musics = await Music.search ({ name: req.params.music_name })
+    
+    return res.send (musics)
+  }
+
+  async store (req: Request<any, Music, MusicRecord>, res: Response<Music>): Promise<Response> {
+
+    const { music: musicDatas } = req.body
+    const music = new Music (musicDatas)
+
+    await music.save ()
+
+    return res.send (music)
+  }
+}
