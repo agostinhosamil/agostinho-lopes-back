48bd62037902a621202148352951be5492f3b604
Agostinho Sam'l <agostinhosaml832@gmail.com>
2021-12-11 22:31:14 +0100

    feat: create MusicController test suite

--- tests/app/controllers/MusicControler.spec.ts
+++ tests/app/controllers/MusicControler.spec.ts
@@ -1,9 +1,56 @@
-import { Music } from '@models/Music'
+import request from 'supertest'
+
+import { app } from '@app'
 
 describe ('Music Creation', () => {
-  it ('should get the music list', () => {
-    const music = new Music ('Ayo')
+  it ('should get the music list', async () => {
+    const response = await request (app)
+      .get ('/musics')
 
-    expect (music.title).toBe ('Ayo')
+    expect (response.status).toBe (200)
+    expect (response.body).toBeInstanceOf (Array)
+  })
+
+  it ('should get a music given an id', async () => {
+    const response = await request (app)
+      .get ('/musics/3')
+
+    expect (response.status).toBe (200)
+    expect (response.body).toBeInstanceOf (Object)
+    expect (response.body).toHaveProperty ('name')
+  })
+
+  it ('should get a music by a name filter', async () => {
+    const response = await request (app)
+      .get ('/musics/autocomplete/Halo')
+
+    expect (response.status).toBe (200)
+    expect (response.body).toBeInstanceOf (Array)
+  })
+
+  it ('should create a user if datas are sent correctly', async () => {
+    const response = await request (app)
+      .post ('/musics')
+      .send ({
+        music: {
+          name: "Ayo",
+          description: "A song from Chris Brown And Tyga, 2011",
+          year: 2011
+        }
+      })
+
+    expect (response.status).toBe (200)
+  })
+
+  it ('should fail creating a user if datas are not sent correctly', async () => {
+    const response = await request (app)
+      .post ('/musics')
+      .send ({
+        name: "Ayo",
+        description: "A song from Chris Brown And Tyga, 2011",
+        year: 2011
+      })
+
+    expect (response.status).toBe (400)
   })
 })
