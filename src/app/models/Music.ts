f951a9bc83df5a3cad02a8ea4fce479052902175
Agostinho Sam'l <agostinhosaml832@gmail.com>
2021-12-11 22:02:27 +0100

    feat: setup editorconfig

--- src/app/models/Music.ts
+++ src/app/models/Music.ts
@@ -1,7 +1,56 @@
-export class Music {
-  title: string = ''
+import knex from '@config/knex'
 
-  constructor (title: string) {
-    this.title = title
+import { AppModel } from '@models/AppModel'
+
+export interface MusicTypes {
+  id?: number
+  name: string
+  year?: number
+  description?: string
+}
+
+export interface MusicRecord {
+  music: MusicTypes
+}
+
+export class Music extends AppModel<MusicTypes> {
+  protected tableName = 'musics'
+  protected static tableName = 'musics'
+
+  protected requireds = ['name']
+  
+  static async all (): Promise<MusicTypes[] | void> {    
+    const musicList = await knex<MusicTypes> (this.tableName)
+      .select ('*')
+    
+    return musicList
   }
- }
+
+  static async search (musicQueryDatas: MusicTypes): Promise<MusicTypes[]> {
+
+    const query = knex (this.tableName)
+      .select ('*')
+
+    Object.keys (musicQueryDatas).map ((key, index) => {
+      const value = `%${musicQueryDatas [key as keyof MusicTypes]}%`
+
+      if (index === 0) {
+        query.where (key, 'like', value)
+      } else {
+        query.andWhere (key, 'like', value)
+      }
+    })
+
+    const queryResultList = await query
+
+    return queryResultList
+  }
+
+  static async findById (id: number): Promise<MusicTypes> {    
+    const musicList = await knex (this.tableName)
+      .select ('*')
+      .where ({ id })
+    
+    return musicList.length >= 1 ? musicList [0] : null
+  }
+}
